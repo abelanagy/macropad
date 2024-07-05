3072a976c7717bf0260e77354f376f442292a50c
Ábel Nagy <abel.nagy2601@gmail.com>
2024-07-05 08:46:01 +0200

    Added a rotary encoder to my test circuit and code

--- code.py
+++ code.py
@@ -6,9 +6,12 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.encoder import EncoderHandler
+
+encoder_handler = EncoderHandler()
 
 keyboard = KMKKeyboard()
-
+keyboard.modules = [encoder_handler]
 keyboard.extensions.append(MediaKeys())
 
 keyboard.col_pins = (board.GP2, board.GP3)
@@ -18,6 +21,11 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 keyboard.keymap = [
     [KC.AUDIO_VOL_DOWN, KC.AUDIO_VOL_UP]
 ]
+
+encoder_handler.pins = (
+    (board.GP17, board.GP16, None,),)
+
+encoder_handler.map = [ (( KC.AUDIO_VOL_DOWN, KC.AUDIO_VOL_UP,),),]
 
 if __name__ == '__main__':
     keyboard.go()
